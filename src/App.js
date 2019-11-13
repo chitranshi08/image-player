e7b38c04fb0427af04b361d756f9266e74cd58cb
chitranshi srivastav <charusrivastav08@gmail.com>
2019-11-13 17:45:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Image from './Image'
+
+function App() {
+  
+   return(
+   	<Image/>
+   	)
+}
+
+export default App;
---
+++ src/Image.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Image.css'
+ export class Image extends React.Component{
+	constructor(){
+	super();
+	this.state = {
+		image:""
+	}
+	}
+	imageClick = (img) => {
+
+		this.setState({
+			
+				image: img
+			
+			
+		})
+	}
+
+render(){
+	const imageList = ["Chrysanthemum.jpg","Desert.jpg","Hydrangeas.jpg","Jellyfish.jpg","Tulips.jpg"]
+	const showimage = imageList.map((image) =>{
+		return <img onClick={() =>this.imageClick(image)} src={"/images/"+image}/>
+	})
+console.log(showimage)
+return(
+	<div className="container">
+	<div className="leftImageContainer">
+	{showimage}
+	</div>
+	<div>
+	<img className="rightContainerImage" src={"/images/"+this.state.image}/>
+	</div>
+	</div>
+	)
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+export default Image
